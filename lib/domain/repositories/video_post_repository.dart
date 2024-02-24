234a1ff5b9d7647d22bda7de59064179c1602a12
Lnx <yermin.linos@ug.edu.ec>
2024-02-24 16:28:15 -0500

    Implementacion ds - repository

--- lib/domain/repositories/video_post_repository.dart
+++ lib/domain/repositories/video_post_repository.dart
@@ -2,5 +2,5 @@ import 'package:toktik/domain/entities/video_post.dart';
 
 abstract class VideoPostRepository {
   Future<List<VideoPost>> getTrendingVideosByPage(int page);
-  Future<List<VideoPost>> getTrendingVideosByUser(String userID);
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID);
 }
---
+++ lib/infraestructure/repositories/video_post_repository_implementation.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/datasources/video_post_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
+class VideoPostsRepository extends VideoPostRepository {
+  final VideoPostDatasource videosDatasource;
+
+  VideoPostsRepository({required this.videosDatasource});
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosDatasource.getTrendingVideosByPage(page);
+  }
+}
